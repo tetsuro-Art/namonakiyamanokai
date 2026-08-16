f143914efd7962840c401ddfcb0e0918f00cde62
哲朗アート <tetsurokarashima0816.o@gmail.com>
2026-08-17 00:23:28 +0900

    Add files via upload

---
+++ kirakira_move.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const video = document.querySelector('.intro-video');
+    if (!video) return;
+
+    video.onloadedmetadata = () => {
+        const duration = video.duration * 1000; // ミリ秒に変換
+        setTimeout(() => {
+            window.location.href = "index.html";
+        }, duration);
+    };
+});
