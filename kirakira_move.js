13d94d848fe00778dcceb01bd775c0ef4615c846
哲朗アート <tetsurokarashima0816.o@gmail.com>
2026-08-17 09:40:13 +0900

    Update kirakira_move.js

--- kirakira_move.js
+++ kirakira_move.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const video = document.querySelector('.intro-video');
+    video.addEventLitener('timeupdate',function onFirstFrame(){
+    if(video.currentTime>0) {
+    console.log('kirakira-start')
     if (!video) return;
 
     video.onloadedmetadata = () => {
@@ -9,18 +12,4 @@ document.addEventListener("DOMContentLoaded", () => {
         }, duration);
     };
 });
-    cost video = document.getElementById('.intro-video');
-    video.addEventLitener('timeupdate',function onFirstFrame(){
-    if(video.currentTime>0) {
-    console.log('kirakira-start')
-
-    if (!video) return;    
-    video.onloadedmetadata = () => {
-        const duration = video.duration * 1000; // ミリ秒に変換
-        setTimeout(() => {
-            window.location.href = "index.html";
-        }, duration);
-    };
-});
- 
 
