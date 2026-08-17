88885385f2b7fefe9154821bef4d02b59cd4ce11
哲朗アート <tetsurokarashima0816.o@gmail.com>
2026-08-17 09:41:24 +0900

    Update kirakira_move.js

--- kirakira_move.js
+++ kirakira_move.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const video = document.querySelector('.intro-video');
-    video.addEventLitener('timeupdate',function onFirstFrame(){
-    if(video.currentTime>0) {
-    console.log('kirakira-start')
     if (!video) return;
 
     video.onloadedmetadata = () => {
